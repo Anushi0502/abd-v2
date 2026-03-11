19e1ad6bcd7720011b5f4953856db31937becb59
anushi0502 <mittalanushi05@gmail.com>
2026-03-11 15:50:54 +0530

    done

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,26 @@ import App from './App'
 import './index.css'
 import './styles/nonHome2026.css'
 
+const ensureAppFavicon = () => {
+  const href = '/abd-favicon.svg?v=20260311b'
+  const links = document.querySelectorAll("link[rel~='icon']")
+
+  if (links.length === 0) {
+    const link = document.createElement('link')
+    link.rel = 'icon'
+    link.type = 'image/svg+xml'
+    link.href = href
+    document.head.appendChild(link)
+    return
+  }
+
+  links.forEach((link) => {
+    link.setAttribute('href', href)
+  })
+}
+
+ensureAppFavicon()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
